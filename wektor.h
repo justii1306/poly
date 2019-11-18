2743c873d182b08e42b6403d366b0df01b48427d
justii1306 <35031460+justii1306@users.noreply.github.com>
2019-11-18 14:30:18 +0100

    Update wektor.h

--- wektor.h
+++ wektor.h
@@ -23,8 +23,6 @@ public:
       return &vec[pos];
     };
     double* insert(double* position, int size, double value){
-      //int pos = vec - position;
-      //std::cout << "Pos: " << pos << std::endl;
       int i = 0;
       for (; i < size; i++){
         vec[i] = value;
@@ -43,16 +41,6 @@ public:
       return j+1;
     };
     double &operator[](int index) {
-        //while (index > vecsize){
-            //int new_size = vecsize * 2;
-            //double* temp = new double[vecsize];
-            //std::memcpy(temp, vec, vecsize*sizeof(double));
-            //vec = new double[new_size];
-            //std::memcpy(vec, temp, vecsize*sizeof(double));
-            //vecsize = new_size;
-            //std::cout << vecsize << std::endl;
-            //delete temp;
-        //}
         return vec[index];
     };
     wektor &operator=(int foo){
